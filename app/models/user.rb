d30c270f0c9faa1807e990c8c1ed4338a3eab440
junohm410 <junohm410@gmail.com>
2024-06-15 10:03:48 +0900

    サーバーメンバーではない場合リクエストに失敗してログインできないようにした
    Discordrb::API::Server.resolve_memberが失敗するかどうかでログインを試みるユーザーがサーバーメンバーかどうかをチェックする

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
     name = auth_hash['info']['name']
     avatar = auth_hash['info']['image']
 
+    # サーバーメンバーではない場合リクエストに失敗してログインできない
+    Discordrb::API::Server.resolve_member("Bot #{ENV['DISCORD_BOT_TOKEN']}", ENV['DISCORD_SERVER_ID'], uid)
+
     User.find_or_create_by!(provider:, uid:) do |user|
       user.name = name
       user.avatar = avatar
